a4878f48c878de94ecd7d593d72117bd1ac40175
RomainNykyforchyn <romainnyky@gmail.com>
2021-11-25 14:12:52 +0100

    validation implementation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,21 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace idpa.Controllers
 {
     public class HomeController : Controller
     {
+
+        private String xmlPath = "/Models/users.xml";
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(String name,  String password)
+        {
+            XDocument doc = XDocument.Load(xmlPath);
+            XmlNode node;
+            return RedirectToAction("/Register");
+        }
 
-        public ActionResult About()
+
+        [HttpGet]
+        public ActionResult Register()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
+        }
+        [HttpPost]
+        public ActionResult Register(String name, String password, String international, String amount, String volume, String provider)
+        {
+            //Declaration
+            int intAmount;
+            Boolean boolInter;
+            int intVolume;
+
+            //Validation
+            try
+            {
+                intAmount = Int32.Parse(amount);
+                intVolume = Int32.Parse(volume);
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+            switch (provider)
+            {
+                case "Swisscom":
+                    break;
+                case "Sunrise": 
+                    break;
+                case "Salt": 
+                    break;
+                case "Yallo": 
+                    break;
+                case "Wingo": 
+                    break;
+                default: return View();
+
+            }
+            
+            if (international.Equals("Ja"))
+            {
+                boolInter = true;
+            }
+            if (international.Equals("Nein"))
+            {
+                boolInter = false;
+            }
+            else
+            {
+                return View();
+            }
+
+            
+            XDocument doc = XDocument.Load(xmlPath);
+            XElement users = doc.Element("users");
+            XElement user = new XElement("user");
+            user.Add(new XElement("name", name), new XElement("password",password), new XElement("international", boolInter), new XElement("amount", intAmount), new XElement("volume", intVolume), new XElement("provider", provider));
+            users.Add(user);
+            /*school.Add(new XElement("Student",
+                       new XElement("FirstName", "David"),
+                       new XElement("LastName", "Smith")));*/
+            doc.Save(xmlPath);
+            return RedirectToAction("/Index");
         }
 
         public ActionResult Contact()
